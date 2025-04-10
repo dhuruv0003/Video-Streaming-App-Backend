398cb2b1c8ef57d0f1bbfafbf11d929ae522ac43
dhuruv0003 <116896454+dhuruv0003@users.noreply.github.com>
2025-04-10 22:54:46 +0530

    new commit

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 import bcrypt from "bcrypt";
-import jwt from "jsonwebtoken ";
+import jwt from "jsonwebtoken";
 
 const userSchema = new mongoose.Schema(
   {
@@ -54,24 +54,39 @@ const userSchema = new mongoose.Schema(
 );
 
 userSchema.pre("save", async function (next) {
-//   if (this.isModified("password")) {
-//     this.password = await bcrypt.hash(this.password, 10);
-//   }
-//   next();
+  //   if (this.isModified("password")) {
+  //     this.password = await bcrypt.hash(this.password, 10);
+  //   }
+  //   next();
   //   or
   if (!this.isModified("password")) {
     return next();
   }
-  this.password=await bcrypt.hash(this.password,10);
-  next()
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
 });
 
-userSchema.methods.isPasswordCorrect=async function(password){
-    return await bcrypt.compare(password,this.password)
-}
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
 
-userSchema.methods.generateAccessToken= function(){
-    
-}
+userSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      username: this.username,
+      fullName: this.fullName,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {expiresIn: process.env.ACCESS_TOKEN_EXPIRY}
+  );
+};
+
+userSchema.methods.generateRefreshToken = function () {
+  return jwt.sign({
+    _id: this._id,
+  });
+};
 
 export const User = mongoose.model("User", userSchema);
